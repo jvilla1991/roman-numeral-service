d9466ffc4f5c0ab10e8cd6ab4c6cf5b4ce2b6398
Jeremy Villa <villajeremy1991@gmail.com>
2023-10-12 16:27:13 -0400

    Finalized and tested Solution one last time

--- src/main/java/com/ford/romannumeralservice/controllers/IntegerController.java
+++ src/main/java/com/ford/romannumeralservice/controllers/IntegerController.java
@@ -5,6 +5,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -15,8 +16,8 @@ public class IntegerController {
     @Autowired
     private IntegerConversionService incService;
 
-    @GetMapping("/convert/{integer}")
-    public ResponseEntity<String> convertRomanNumeralToInteger(Integer number) {
+    @GetMapping("/convert/{number}")
+    public ResponseEntity<String> convertRomanNumeralToInteger(@PathVariable Integer number) {
         String output = incService.processIntegerToRomanNumeral(number);
         return new ResponseEntity<>(output, HttpStatus.OK);
     }
--- src/main/java/com/ford/romannumeralservice/controllers/RomanNumeralController.java
+++ src/main/java/com/ford/romannumeralservice/controllers/RomanNumeralController.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -19,7 +20,7 @@ public class RomanNumeralController {
     private RomanNumeralConversionService rncService;
 
     @GetMapping("/convert/{romanNumeral}")
-    public ResponseEntity<Integer> convertRomanNumeralToInteger(String romanNumeral) {
+    public ResponseEntity<Integer> convertRomanNumeralToInteger(@PathVariable String romanNumeral) {
         Integer output = rncService.processRomanNumeralToInteger(romanNumeral);
         return new ResponseEntity<>(output, HttpStatus.OK);
     }
